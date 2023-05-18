97012ea7d17cf44fe14c27c26356b1bdc982f6b7
mahmudev <jonas67028@gmail.com>
2023-05-18 11:25:51 +0600

    get route done

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djzcyyl.mongodb.net/`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const productsCollection = client.db("funkoFanfare").collection("products");
+    app.get("/products", async (req, res) => {
+      let { page, limit, sort, asc } = req.query;
+      if (!page) page = 1;
+      if (!limit) limit = 5;
+      limit = parseInt(limit);
+      const skip = (page - 1) * 5;
+      const products = await productsCollection
+        .find()
+        .sort({ [sort]: asc })
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await productsCollection
+        .find({
+          $or: [{ toyName: { $regex: text, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+    app.get("/products/category/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await productsCollection
+        .find({
+          $or: [{ category: { $regex: text, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
