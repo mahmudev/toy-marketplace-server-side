c23da4dabf3d872e23d346f727dd7915049d4b32
mahmudev <jonas67028@gmail.com>
2023-05-18 12:03:49 +0600

    added delete route

--- index.js
+++ index.js
@@ -99,6 +99,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
