c0da077251502616e21e1b3b8f11f117dd0dfefe
mahmudev <jonas67028@gmail.com>
2023-05-20 21:40:49 +0600

    fixed bugs

--- index.js
+++ index.js
@@ -5,104 +5,79 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djzcyyl.mongodb.net/`;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djzcyyl.mongodb.net/`;
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, }});
 
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
-    return res
-      .status(401)
-      .send({ error: true, message: "unauthorized access" });
-  }
+    return res.status(401).send({ error: true, message: "unauthorized access" });
+}
   const token = authorization.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res
-        .status(401)
-        .send({ error: true, message: "unauthorized access" });
+      return res.status(401).send({ error: true, message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  });
-};
+  });};
 
 async function run() {
   try {
-    const productsCollection = client.db("funkoFanfare").collection("products");
+    const toysCollection = client.db("funkoFanfare").collection("products");
 
     app.post("/jwt", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "30d",
-      });
+      const token = jwt.sign(req.body, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "30d",});
       res.send({ token });
     });
 
-    app.get("/products", async (req, res) => {
-      let { page, limit, sort, asc } = req.query;
+    app.get("/toys", async (req, res) => {
+      let { page, limit, } = req.query;
       if (!page) page = 1;
-      if (!limit) limit = 5;
+      if (!limit) limit = 20;
       limit = parseInt(limit);
-      const skip = (page - 1) * 5;
-      const products = await productsCollection
-        .find()
-        .sort({ [sort]: asc })
-        .skip(skip)
-        .limit(limit)
-        .toArray();
+      const skip = (page - 1) * 20;
+      const products = await toysCollection.find().skip(skip).limit(limit).toArray();
       res.send(products);
     });
-    app.get("/products/:id", async (req, res) => {
+
+    app.get("/toys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.findOne(query);
+      const result = await toysCollection.findOne(query);
       res.send(result);
     });
 
-    app.get("/products/toy-name/:text", async (req, res) => {
+    app.get("/toys/toy-name/:text", async (req, res) => {
       const text = req.params.text;
-      const result = await productsCollection
-        .find({
-          $or: [{ toyName: { $regex: text, $options: "i" } }],
-        })
-        .toArray();
-      res.send(result);
-    });
-    app.get("/products/category/:text", async (req, res) => {
-      const text = req.params.text;
-      const result = await productsCollection
-        .find({
-          $or: [{ category: { $regex: text, $options: "i" } }],
-        })
-        .toArray();
-      res.send(result);
-    });
-
-
-    app.get("/added-toys", verifyJWT, async (req, res) => {
-      let query = {};
-      if (req.query?.email) {
-        query = { sellerEmail: req.query.email };
-      }
-      const result = await productsCollection.find(query).toArray();
+      const result = await toysCollection.find({ $or: [{ toyName: { $regex: text, $options: "i" } }]}).toArray();
       res.send(result);
     });
     
-    app.post("/added-toys", async (req, res) => {
+    app.get("/toys/category/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await toysCollection.find({ $or: [{ category: { $regex: text, $options: "i" } }]}).toArray();
+      res.send(result);
+    });
+
+    app.get("/added-toys", verifyJWT, async (req, res) => {
+      const sortOrder = req.query.sort;
+      let sortOption = {};
+      if (sortOrder === "asc") { sortOption = { Price: 1 }}
+      else if (sortOrder === "desc") { sortOption = { Price: -1 }}
+      let query = {};
+      if (req.query?.email) { query = { sellerEmail: req.query.email }}
+      const result = await toysCollection.find(query).sort(sortOption).toArray();
+      res.send(result);
+    });
+
+    app.post("/add-toys", async (req, res) => {
       const newProduct = req.body;
-      const result = await productsCollection.insertOne(newProduct);
+      const result = await toysCollection.insertOne(newProduct);
       res.send(result);
     }); 
 
@@ -128,31 +103,24 @@ async function run() {
           material: updatedProduct.material,
         },
       };
-      const result = await productsCollection.updateOne(
-        filter,
-        product,
-        options
-      );
+      const result = await toysCollection.updateOne(filter,product,options);
       res.send(result);
     });
 
     app.delete("/added-toys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.deleteOne(query);
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+  } finally {}
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
